5d180b9fc76512c85871653374700ad45026ae4b
Mark <mark@greenorange.com>
2016-09-22 14:47:23 +0200

    hooooi

--- echoserver.py
+++ echoserver.py
@@ -57,7 +57,7 @@ def send_message(token, recipient, text):
   print(response)
   for part in response:
       print(part)
-  if 'text' in response':
+  if 'text' in response:
       r = requests.post("https://graph.facebook.com/v2.6/me/messages",
         params={"access_token": token},
         data=json.dumps({
