e08ba79240eb56aa8d6c63de796d5dbe3b7d07ff
Mark <mark@greenorange.com>
2016-09-22 10:24:37 +0200

    jajaja

--- echoserver.py
+++ echoserver.py
@@ -52,10 +52,11 @@ def messaging_events(payload):
 
 
 def send_message(token, recipient, text):
+	
   """Send the message text to recipient with id recipient.
   """
+	
 	msg = client.message(text)
-
   r = requests.post("https://graph.facebook.com/v2.6/me/messages",
     params={"access_token": token},
     data=json.dumps({
