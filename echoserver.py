5b827b114642beaa9159c252ce320a75829e5109
Mark <mark@greenorange.com>
2016-09-22 14:13:14 +0200

    hooooi

--- echoserver.py
+++ echoserver.py
@@ -54,7 +54,7 @@ def send_message(token, recipient, text):
 
   #print(response['text'])
   response = tb.response(text)
-
+  print(response)
   print(response['confidence'])
   r = requests.post("https://graph.facebook.com/v2.6/me/messages",
     params={"access_token": token},
