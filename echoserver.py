a68916f681a2bc727f8b5bd0e1516643e5fec1df
Mark <mark@greenorange.com>
2016-09-22 09:24:16 +0200

    jajaja

--- echoserver.py
+++ echoserver.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, request
 import json
 import requests
@@ -7,6 +8,7 @@ app = Flask(__name__)
 # This needs to be filled with the Page Access Token that will be provided
 # by the Facebook App that will be created.
 PAT = 'EAAEkTt8L730BAJzPxFYza8w3Ob9SlH41MwZArFoLFdGCSpgPYkoOB2zfIOJnaDhhP922PyEIayJH5HpzMKZCGM0IcbvZBZCrKRaFY1tj27pGsFcAu2KzvO8ZCusT5OvsUG9RghmR9UDMIOND2prsW5RL4taRe15YgZAtwrgRsM1QZDZD'
+
 
 @app.route('/', methods=['GET'])
 def handle_verification():
@@ -40,6 +42,20 @@ def messaging_events(payload):
     else:
       yield event["sender"]["id"], "I can't echo this"
 
+
+def send_message(token, recipient, text):
+  """Send the message text to recipient with id recipient.
+  """
+
+  r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+    params={"access_token": token},
+    data=json.dumps({
+      "recipient": {"id": recipient},
+      "message": {"text": text.decode('unicode_escape')}
+    }),
+    headers={'Content-type': 'application/json'})
+  if r.status_code != requests.codes.ok:
+    print r.text
 
 if __name__ == '__main__':
   app.run()
