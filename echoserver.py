d5fb05f51e558fd003dcd688f08e9b7fe9ba977a
Mark <mark@greenorange.com>
2016-09-22 14:40:42 +0200

    hooooi

--- echoserver.py
+++ echoserver.py
@@ -58,15 +58,16 @@ def send_message(token, recipient, text):
   print(response['text'])
   for part in response:
       print(part)
-  r = requests.post("https://graph.facebook.com/v2.6/me/messages",
-    params={"access_token": token},
-    data=json.dumps({
-      "recipient": {"id": recipient},
-      "message": {"text": response['text'].decode('unicode_escape')}
-    }),
-    headers={'Content-type': 'application/json'})
-  if r.status_code != requests.codes.ok:
-    print r.response
+  if reponse['text']:
+      r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+        params={"access_token": token},
+        data=json.dumps({
+          "recipient": {"id": recipient},
+          "message": {"text": response.decode('unicode_escape')}
+        }),
+        headers={'Content-type': 'application/json'})
+      if r.status_code != requests.codes.ok:
+        print r.response
 
 if __name__ == '__main__':
   app.run()
