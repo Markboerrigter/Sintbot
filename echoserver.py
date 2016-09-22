aed03bd0fe1dd1b3e39fddf4641033b1859ad132
Mark <mark@greenorange.com>
2016-09-22 09:19:59 +0200

    jajaja

--- echoserver.py
+++ echoserver.py
@@ -2,7 +2,6 @@ from flask import Flask, request
 import json
 import requests
 
-
 app = Flask(__name__)
 
 # This needs to be filled with the Page Access Token that will be provided
@@ -41,20 +40,6 @@ def messaging_events(payload):
     else:
       yield event["sender"]["id"], "I can't echo this"
 
-
-def send_message(token, recipient, text):
-  """Send the message text to recipient with id recipient.
-  """
-
-  r = requests.post("https://graph.facebook.com/v2.6/me/messages",
-    params={"access_token": token},
-    data=json.dumps({
-      "recipient": {"id": recipient},
-      "message": {"text": resp(text).decode('unicode_escape')}
-    }),
-    headers={'Content-type': 'application/json'})
-  if r.status_code != requests.codes.ok:
-    print r.text
 
 if __name__ == '__main__':
   app.run()
