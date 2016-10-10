5b82a919712067cf5ea92688836b658949412a31
root <mark@greenorange.com>
2016-10-10 12:57:10 +0000

    liefdeee

--- echoserver.py
+++ echoserver.py
@@ -131,7 +131,7 @@ def mergeAns(response, witToken, session_id):
 
             print(response['entities'])
             text = ''
-            return tb.response(text, witToken, session_id, tb.merge(response))
+            return tb.response(text, witToken, session_id,{})
         else:
             return response
     else:
