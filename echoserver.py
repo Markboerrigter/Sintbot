ded83f03a856be82dc7c5e4f4d87319820521b6d
root <mark@greenorange.com>
2016-10-17 14:00:00 +0000

    liefdeee

--- echoserver.py
+++ echoserver.py
@@ -287,9 +287,9 @@ def send_message(token, recipient, text, data):
       Stage = get_keys(Tokens, oldToken)[0]
       if TokenStages.index(Stage) < len(TokenStages)-1:
           NextStage = TokenStages[TokenStages.index(Stage)+1]
-          data['token'] = random.choice(allValues(Tokens[NextStage]))
-        #   if isinstance(data['token'], dict):
-        #       data['token'] = random.choice(allValues(data['token']))
+          data['token'] = Tokens[NextStage]
+          if isinstance(data['token'], dict):
+              data['token'] = random.choice(allValues(data['token']))
           token = data['token']
           print(token)
           response, data = getResponse(recipient, text, data)
