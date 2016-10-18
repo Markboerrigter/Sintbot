ae4ec401986e9bb3b4190f0074cb1a6c37efc38e
root <mark@greenorange.com>
2016-10-18 14:05:33 +0000

    liefdeee

--- echoserver.py
+++ echoserver.py
@@ -148,7 +148,6 @@ def handle_messages():
   print('message events')
   for sender, message in messaging_events(payload):
     print(sender, message)
-    checksuggest(PAT, sender, {})
     if sender in user_data:
         print("Incoming from %s: %s" % (sender, message))
         print(sender, message)
