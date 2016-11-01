3f315db38a3de9ed2d1d716ccd93e2e905c87128
root <mark@greenorange.com>
2016-11-01 16:05:12 +0000

    liefdeee

--- echoserver.py
+++ echoserver.py
@@ -735,7 +735,20 @@ def send_message(token, recipient, text, data):
 
   elif data['Stage'] == 'Personality':
 
-
+    if len(data['personality']) == 0:
+        message = 'Om je wat beter te leren kennen stel ik je nu een paar vragen.'
+        data['text'].append(('bot',message))
+        data['oldmessage'] = message
+        postdashbot('bot',(recipient,message, data['message-id']) )
+        typing('off', PAT, recipient)
+        r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+            params={"access_token": token},
+            data=json.dumps({
+              "recipient": {"id": recipient},
+              "message":message}),
+            headers={'Content-type': 'application/json'})
+        if r.status_code != requests.codes.ok:
+            	print r.text
     data['personality'].append(text)
     if len(data['personQuestions']) > 2:
         response, data = findToken(recipient, data, text)
