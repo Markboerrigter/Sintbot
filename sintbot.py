03bb41ab42db39a1f0cf29d55bc5e798e4e21384
Leo Bekhuis <lbekhuis@gmail.com>
2016-11-04 11:42:52 +0100

    issue line 722 childtypes

--- sintbot.py
+++ sintbot.py
@@ -719,7 +719,7 @@ def messaging_events(payload):
         # if "messaging" in event and "attachment" in event["messaging"][0] and event["messaging"][0]["message"]['attachment']['payload']['elements'][0]['buttons'][1]['type'] == 'postback':
         #   yield event["messaging"][0]["recipient"]['id'], event["messaging"][0]["message"]['attachment']['payload']['elements'][0]['buttons'][1]['title'].encode('unicode_escape'), event["messaging"][0]["message"]['mid'], event['messaging'][0]['recipient']['id']
 
-print(childTypes[9])
+# print(childTypes[9])
 
 def send_message(token, recipient, text, data):
   """Send the message text to recipient with id recipient.
