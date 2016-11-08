4f84ec9ac75ff19d2d8a0271e45156fcef6699c1
root <mark@greenorange.com>
2016-11-08 12:10:18 +0000

    liefdeee

--- sintbot.py
+++ sintbot.py
@@ -37,7 +37,7 @@ app = Flask(__name__)
 
 channel = 'sintbot'
 if channel == 'sintbot':
-    dashbotAPI, PAT, N = os.environ['dashbotAPI'], os.environ['PAT1'], os.environ['N']
+    dashbotAPI, PAT, N = os.environ['dashbotAPI'], os.environ['PAT1'], int(os.environ['N'])
 else: dashbotAPI, PAT, N = os.environ['dashbotAPI'], os.environ['PAT2'], int(os.environ['N'])
 
 """ FORMULAS ON TEXT PROCESSING
