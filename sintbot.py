25997492d5f7c8f114bfe446db604c27684d276c
root <mark@greenorange.com>
2016-11-09 15:32:23 +0000

    liefdeee

--- sintbot.py
+++ sintbot.py
@@ -328,12 +328,12 @@ def checksuggest(token, recipient, data,n):
         for x in presents:
             if x['retailer'] == 'intertoys':
                 x['item_url'] = "https://www.spotta.nl/folders/intertoys?fid=1&page=" + str(x['page'])
-                if not x['img_link']:
+                if not  x['img_link'].startswith('http'):
                     print('no pic')
                     x.update({'img_link':'https://support.greenorange.com/sint/intertoys/'+ 'p' + str(x['page']) + '_' + str(x['article_number']) + '.png'})
             else:
                 x['item_url'] = "https://www.spotta.nl/folders/bart-smit?fid=116&page=" + str(x['page'])
-                if not x['img_link']:
+                if not x['img_link'].startswith('http'):
                     print('no pic')
                     x.update({'img_link':'https://support.greenorange.com/sint/bartsmit/'+ 'p' + str(x['page']) + '-' + str(x['article_number']) + '.jpg'})
             newpres.append(x)
