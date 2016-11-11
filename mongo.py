bbb4e6d33fe7d620a2d20a60a0f0cf5eb89baf89
root <mark@greenorange.com>
2016-11-11 13:16:50 +0000

    liefdeee

--- mongo.py
+++ mongo.py
@@ -1021,8 +1021,10 @@ def findRightProduct(geslacht, budget, age, category, idea,n):
     if len(budget) >1:
         budgetQuery = findFromRange(budget[0],budget[1])
     else:
+        if int(budget[0]):
+            budgetQuery = findAbovevalue(budget[0])
         budget = [int(s) for s in budget.split() if s.isdigit()]
-        budgetQuery = findAbovevalue(budget)
+        budgetQuery = findAbovevalue(budget[0])
     ageQuery = findByAge(age)
     if idea == '':
         ideaStem = 'jaa'
