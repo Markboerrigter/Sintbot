1c90688d9ff79dbb14de5f763a177425bd237407
root <mark@greenorange.com>
2016-11-21 09:58:19 +0000

    liefdeee

--- sintbot.py
+++ sintbot.py
@@ -361,20 +361,20 @@ def checksuggest(token, recipient, data,n):
             presents = [x for x in presents if x not in data['presented']]
         newpres = []
         print('I found' + str(len(presents)) + 'presents')
-        # for x in presents:
-        #     if x['retailer'] == 'intertoys':
-        #         x['item_url'] = "https://www.spotta.nl/folders/intertoys?fid=1&page=" + str(x['page'])
-        #         if not  x['img_link'].startswith('http'):
-        #             print('no pic')
-        #             x.update({'img_link':'https://support.greenorange.com/sint/intertoys/'+ 'p' + str(x['page']) + '-' + str(x['article_number']) + '.png'})
-        #     else:
-        #         x['item_url'] = "https://www.spotta.nl/folders/bart-smit?fid=116&page=" + str(x['page'])
-        #         if not x['img_link'].startswith('http'):
-        #             print('no pic')
-        #             x.update({'img_link':'https://support.greenorange.com/sint/bartsmit/'+ 'p' + str(x['page']) + '_' + str(x['article_number']) + '.png'})
-        #     newpres.append(x)
+        for x in presents:
+            if x['retailer'] == 'intertoys':
+                x['item_url'] = "https://www.spotta.nl/folders/intertoys?fid=1&page=" + str(x['page'])
+                # if not  x['img_link'].startswith('http'):
+                #     print('no pic')
+                #     x.update({'img_link':'https://support.greenorange.com/sint/intertoys/'+ 'p' + str(x['page']) + '-' + str(x['article_number']) + '.png'})
+            else:
+                x['item_url'] = "https://www.spotta.nl/folders/bart-smit?fid=116&page=" + str(x['page'])
+                # if not x['img_link'].startswith('http'):
+                #     print('no pic')
+                #     x.update({'img_link':'https://support.greenorange.com/sint/bartsmit/'+ 'p' + str(x['page']) + '_' + str(x['article_number']) + '.png'})
+            newpres.append(x)
         print(N)
-        presents = presents[:N]
+        presents = newpres[:N]
         print(len(presents))
         postdashbot('bot',(recipient,'presents', data['message-id']) )
         data['presented'].extend(presents)
