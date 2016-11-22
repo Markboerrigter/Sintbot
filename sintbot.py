fa087dcaa66c0f238c53a840205e3e4afd1dd8a0
root <mark@greenorange.com>
2016-11-22 13:13:20 +0000

    liefdeee

--- sintbot.py
+++ sintbot.py
@@ -900,6 +900,9 @@ def handle_messages():
   print(payload)
   for sender, message, mid, recipient in messaging_events(payload) :
     try:
+        if sender == 1042410335857237:
+            Error
+        
         print("Incoming from %s: %s" % (sender, message))
         postdashbot('human', payload)
         if not mg.findUser(sender):
